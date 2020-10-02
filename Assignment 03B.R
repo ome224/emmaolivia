37c862e7932c3b69d20835d52c58167c706cb4b7
Olivia Emma <ome224@lehigh.edu>
2020-10-01 21:55:18 -0400

    Assignment 03B.R

---
+++ Assignment 03B.R
@@ -0,0 +1,11 @@
+load("/Users/oliviaemma/Downloads/prrace08.rda")
+
+
+"p_obama" <- 0 # Number of "el_votes"
+"p_mc_cain" <- 0 # Number of "el_votes"
+
+if ( "p_obama" > "p_mc_cain"){
+  print("Obama wins")
+} else {
+  print("McCain wins")
+}
