bde72681cb5221f3191cad843e9b3ff5072eb6fb
Olivia Emma <ome224@lehigh.edu>
2020-10-01 22:36:05 -0400

    made changes to code

--- Assignment 03B.R
+++ Assignment 03B.R
@@ -4,6 +4,10 @@ load("/Users/oliviaemma/Downloads/prrace08.rda")
 "p_obama" <- 0 # Number of "el_votes"
 "p_mc_cain" <- 0 # Number of "el_votes"
 
+for ("el_votes" in "prrace08") {
+  
+}
+
 if ( "p_obama" > "p_mc_cain"){
   print("Obama wins")
 } else {
