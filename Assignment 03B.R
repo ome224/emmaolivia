a3b52fc22170b0088879b52c5d73aade4fc7ccca
Olivia Emma <ome224@lehigh.edu>
2020-10-02 00:52:44 -0400

    more changes, added cat command so that votes can be summarized, and took into account +1 vote for Obama/ -1 vote for Mccain

--- Assignment 03B.R
+++ Assignment 03B.R
@@ -1,15 +1,20 @@
 load("/Users/oliviaemma/Downloads/prrace08.rda")
 
 
-"p_obama" <- 0 # Number of "el_votes"
-"p_mc_cain" <- 0 # Number of "el_votes"
+"p_obama" <- 0
+"p_mc_cain" <- 0
 
-for ("el_votes" in "prrace08") {
-  
-}
+for (i in 1:51)
 
-if ( "p_obama" > "p_mc_cain"){
+if(prrace08$p_obama[i] > prrace08$p_mc_cain[i]){
+  obama_el_votes = obama_el_votes + prrace08$el_votes
   print("Obama wins")
-} else {
-  print("McCain wins")
-}
+}else
+  mccain_el_votes = mccain_vel_otes + prrace08$el_votes[i]
+print("Mccain wins")
+cat("obama: ")
+obama_el_votes = obama_el_votes + 1
+cat(obama_el_votes)
+cat("mccain: ")
+mccain_el_votes = mccain_el_votes - 1
+cat(mccain_el_votes)
