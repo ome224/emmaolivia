26b6c48edcff708503c5c16d9aa49e1999290127
Olivia Emma <ome224@lehigh.edu>
2020-10-02 08:56:09 -0400

    more changes

--- Assignment 03B.R
+++ Assignment 03B.R
@@ -1,5 +1,7 @@
 load("/Users/oliviaemma/Downloads/prrace08.rda")
 
+365 = "obama_votes"
+173 = "mccain_votes"
 
 "p_obama" <- 0
 "p_mc_cain" <- 0
@@ -7,14 +9,14 @@ load("/Users/oliviaemma/Downloads/prrace08.rda")
 for (i in 1:51)
 
 if(prrace08$p_obama[i] > prrace08$p_mc_cain[i]){
-  obama_el_votes = obama_el_votes + prrace08$el_votes
+  obama_votes = obama_votes + prrace08$el_votes
   print("Obama wins")
 }else
-  mccain_el_votes = mccain_vel_otes + prrace08$el_votes[i]
+  mccain_votes = mccain_votes + prrace08$el_votes[i]
 print("Mccain wins")
 cat("obama: ")
-obama_el_votes = obama_el_votes + 1
-cat(obama_el_votes)
+obama_votes = "obama_votes" + 1
+cat(obama_votes)
 cat("mccain: ")
-mccain_el_votes = mccain_el_votes - 1
-cat(mccain_el_votes)
+mccain_votes = "mccain_votes" - 1
+cat(mccain_votes)
